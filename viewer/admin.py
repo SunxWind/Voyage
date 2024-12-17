f24340cf786c77cf330cc3750a7913c019daca9b
Vladimir <mr.symonov@gmail.com>
2024-12-17 16:53:00 +0100

    change (DB.1): Continent model is deleted. Continent CharField with choise is added to County model

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from viewer.models import Country, Hotel, Airport, Trip  # Continent,
 
 # Register your models here.
+# admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(Hotel)
+admin.site.register(Airport)
+admin.site.register(Trip)
--- viewer/models.py
+++ viewer/models.py
@@ -9,19 +9,59 @@ from django.db.models import (
 # Create your models here.
 
 
-class Continent(Model):
-    name = CharField(max_length=12)
-
-    def __str__(self):
-        return self.name
+# class Continent(Model):
+#     N_A = '------'
+#     AF = 'Africa'
+#     AN = 'Antarctica'
+#     AS = 'Asia'
+#     EU = 'Europe'
+#     NA = 'North America'
+#     OC = 'Australia & Oceania'
+#     SA = 'South America'
+#
+#     CHOICES = (
+#         (N_A, '------'),
+#         (AF, 'Africa'),
+#         (AN, 'Antarctica'),
+#         (AS, 'Asia'),
+#         (EU, 'Europe'),
+#         (NA, 'North America'),
+#         (OC, 'Australia & Oceania'),
+#         (SA, 'South America')
+#     )
+#
+#     name = CharField(max_length=20, choices=CHOICES, default=N_A)
+#
+#     def __str__(self):
+#         return self.name
 
 
 class Country(Model):
+    N_A = '------'
+    AF = 'Africa'
+    AN = 'Antarctica'
+    AS = 'Asia'
+    EU = 'Europe'
+    NA = 'North America'
+    OC = 'Australia & Oceania'
+    SA = 'South America'
+
+    CHOICES = (
+        (N_A, '------'),
+        (AF, 'Africa'),
+        (AN, 'Antarctica'),
+        (AS, 'Asia'),
+        (EU, 'Europe'),
+        (NA, 'North America'),
+        (OC, 'Australia & Oceania'),
+        (SA, 'South America')
+    )
+
     name = CharField(max_length=30)
-    continent = ForeignKey(Continent, on_delete=DO_NOTHING, default=None)
+    continent = CharField(max_length=20, choices=CHOICES, default=N_A)
 
     def __str__(self):
-        return f"{self.name} ({self.continent.name})"
+        return f"{self.name} ({self.continent})"
 
 
 class City(Model):
@@ -50,15 +90,29 @@ class Airport(Model):
 
 
 class Trip(Model):
+
+    NA = '--'
+    BB = 'BB'
+    HB = 'HB'
+    FB = 'FB'
+    AI = 'AI'
+
+    CHOICES = (
+        (NA, '--'),
+        (BB, 'BB'),
+        (HB, 'HB'),
+        (FB, 'FB'),
+        (AI, 'AI'),
+    )
+
     code = CharField(max_length=7)
-    name = CharField(max_length=50)
     where_from = ForeignKey(Airport, on_delete=DO_NOTHING, related_name="where_from", default=None)
     where_to = ForeignKey(Airport, on_delete=DO_NOTHING, default=None)
     where_to_hotel = ForeignKey(Hotel, on_delete=DO_NOTHING, related_name="where_to", default=None)
     departure_date = DateField(default=None)
     return_date = DateField(default=None)
     duration = IntegerField(default=None, null=False)
-    type = CharField(max_length=2)
+    type = CharField(max_length=20, choices=CHOICES, default=None)
     adult_price = DecimalField(max_digits=8, decimal_places=2)
     child_price = DecimalField(max_digits=8, decimal_places=2)
     promoted = BooleanField(default=False)
